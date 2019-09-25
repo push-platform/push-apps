d46a5ed040632fa7634329a3ab20cb89c3241021
Matheus <matheus.msa27@gmail.com>
2019-09-25 11:28:47 -0300

    perspective for pushinho channel tests

---
+++ general/channels/types/pushinho/tests.py
@@ -0,0 +1,22 @@
+from django.urls import reverse
+
+from temba.tests import TembaTest
+
+from ...models import Channel
+
+
+class PushinhoTypeTest(TembaTest):
+
+    def test_create_pushinho_channel(self):
+        pass
+        #verify informations created
+
+    def test_update_pushinho_channel(self):
+        pass
+        #verify informations updated
+
+
+class UploadFileToAWS(TembaTest):
+
+    def test_upload_file_to_aws(self):
+        pass
