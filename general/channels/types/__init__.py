e2bba485bff839d65d23b4f4b6a863afa1b86809
Matheus <matheus.msa27@gmail.com>
2019-09-25 10:14:21 -0300

    lint on python files

--- general/channels/types/__init__.py
+++
@@ -1,1 +0,0 @@
-from ....general.channels import types
--- views.py
+++ views.py
@@ -1,18 +1,5 @@
-from urllib.parse import parse_qs, urlencode
-
-from smartmin.views import SmartCreateView, SmartCRUDL, SmartFormView, SmartListView, SmartReadView, SmartTemplateView
-
-from django.conf import settings
-from django.http import HttpResponse, HttpResponseRedirect
-from django.urls import reverse
-from django.utils.translation import ugettext_lazy as _
-from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import RedirectView, View
-
-from temba.apks.models import Apk
-from temba.public.models import Lead, Video
-from temba.utils import analytics, get_anonymous_user, json
-from temba.utils.text import random_string
+from urllib.parse import parse_qs
+from smartmin.views import SmartTemplateView
 
 
 class IndexView(SmartTemplateView):
