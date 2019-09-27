203d324a74e595ab73a1e8154b412ad28ac359a1
Matheus <matheus.msa27@gmail.com>
2019-09-27 10:53:01 -0300

    run black on tests files

--- general/channels/types/pushinho/tests.py
+++ general/channels/types/pushinho/tests.py
@@ -32,8 +32,12 @@ class PushinhoTypeTest(TembaTest):
         response = self.client.get(url)
         post_data = response.context["form"].initial
 
-        main_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb")
-        chat_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb")
+        main_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb"
+        )
+        chat_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb"
+        )
 
         post_data["channel_name"] = "The New Channel"
         post_data["main_icon"] = SimpleUploadedFile(main_icon.name, main_icon.read())
@@ -55,10 +59,14 @@ class PushinhoTypeTest(TembaTest):
         self.assertEqual(pushinho_channel.org, self.user.get_org())
         self.assertEqual(pushinho_channel.address, "The New Channel")
         self.assertEqual(pushinho_channel.channel_type, PushinhoType.code)
-        self.assertEqual(pushinho_channel.role, Channel.ROLE_SEND + Channel.ROLE_RECEIVE)
+        self.assertEqual(
+            pushinho_channel.role, Channel.ROLE_SEND + Channel.ROLE_RECEIVE
+        )
         self.assertEqual(pushinho_channel.schemes, [EXTERNAL_SCHEME])
         self.assertTrue(pushinho_channel.config)
-        self.assertEqual(pushinho_channel.config.get("send_url"), settings.PUSH_WEB_SOCKET_URL)
+        self.assertEqual(
+            pushinho_channel.config.get("send_url"), settings.PUSH_WEB_SOCKET_URL
+        )
 
         config_url = reverse("pushinho_configuration", args=[pushinho_channel.uuid])
         self.assertRedirect(response, config_url)
@@ -76,8 +84,12 @@ class PushinhoTypeTest(TembaTest):
         response = self.client.get(url)
         post_data = response.context["form"].initial
 
-        main_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb")
-        chat_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb")
+        main_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb"
+        )
+        chat_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb"
+        )
 
         post_data["channel_name"] = "The New Channel"
         post_data["main_icon"] = SimpleUploadedFile(main_icon.name, main_icon.read())
@@ -101,14 +113,29 @@ class PushinhoTypeTest(TembaTest):
         post_data_initial = response.context["form"].initial
 
         self.assertEqual(post_data_initial.get("name"), pushinho_channel.name)
-        self.assertEqual(post_data_initial.get("config").get("main_icon_color"), "#18fae2")
-        self.assertEqual(post_data_initial.get("config").get("chat_icon_color"), "#a40aee")
-        self.assertEqual(post_data_initial.get("config").get("chat_push_message_color"), "#23a519")
-        self.assertEqual(post_data_initial.get("config").get("chat_push_text_color"), "#dbf609")
-        self.assertEqual(post_data_initial.get("config").get("chat_user_text_color"), "#e2e0f0")
-        self.assertEqual(post_data_initial.get("config").get("auto_open"), 'true')
-        self.assertEqual(post_data_initial.get("config").get("welcome_button"), "welcome_keyword")
-        self.assertEqual(post_data_initial.get("config").get("welcome_message"), "Welcome to a Channel")
+        self.assertEqual(
+            post_data_initial.get("config").get("main_icon_color"), "#18fae2"
+        )
+        self.assertEqual(
+            post_data_initial.get("config").get("chat_icon_color"), "#a40aee"
+        )
+        self.assertEqual(
+            post_data_initial.get("config").get("chat_push_message_color"), "#23a519"
+        )
+        self.assertEqual(
+            post_data_initial.get("config").get("chat_push_text_color"), "#dbf609"
+        )
+        self.assertEqual(
+            post_data_initial.get("config").get("chat_user_text_color"), "#e2e0f0"
+        )
+        self.assertEqual(post_data_initial.get("config").get("auto_open"), "true")
+        self.assertEqual(
+            post_data_initial.get("config").get("welcome_button"), "welcome_keyword"
+        )
+        self.assertEqual(
+            post_data_initial.get("config").get("welcome_message"),
+            "Welcome to a Channel",
+        )
 
         # Update a object with view for update
         post_data["main_icon"] = None
@@ -144,8 +171,12 @@ class PushinhoTypeTest(TembaTest):
         response = self.client.get(url)
         post_data = response.context["form"].initial
 
-        main_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb")
-        chat_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb")
+        main_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb"
+        )
+        chat_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb"
+        )
 
         post_data["channel_name"] = "The New Channel"
         post_data["main_icon"] = SimpleUploadedFile(main_icon.name, main_icon.read())
@@ -159,7 +190,7 @@ class PushinhoTypeTest(TembaTest):
         post_data["welcome_message"] = "Welcome to a Channel"
 
         response = self.client.post(url, post_data)
-        self.assertTrue(response.context.get('form').errors)
+        self.assertTrue(response.context.get("form").errors)
 
     def test_configuration_pushinho_channel(self):
         url = reverse("channels.types.pushinho.claim")
@@ -176,8 +207,12 @@ class PushinhoTypeTest(TembaTest):
         response = self.client.get(url)
         post_data = response.context["form"].initial
 
-        main_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb")
-        chat_icon = open(os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb")
+        main_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_one.png"), "rb"
+        )
+        chat_icon = open(
+            os.path.join(self.BASE_DIR, "pushinho/test_files/profile_two.png"), "rb"
+        )
 
         post_data["channel_name"] = "The New Channel"
         post_data["main_icon"] = SimpleUploadedFile(main_icon.name, main_icon.read())
@@ -194,21 +229,23 @@ class PushinhoTypeTest(TembaTest):
         response = self.client.post(url, post_data)
         channel_updated = Channel.objects.get()
 
-        response = self.client.get(reverse("pushinho_configuration", args=[channel_updated.uuid]))
+        response = self.client.get(
+            reverse("pushinho_configuration", args=[channel_updated.uuid])
+        )
         self.assertEqual(response.status_code, 200)
 
 
 class UploadFileToAWS(TembaTest):
-
     def test_upload_file_to_aws(self):
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        main_icon = open(os.path.join(BASE_DIR, "pushinho/test_files/profile_one.png"), "rb")
+        main_icon = open(
+            os.path.join(BASE_DIR, "pushinho/test_files/profile_one.png"), "rb"
+        )
 
         self.assertTrue(upload_icon_to_aws(main_icon))
 
 
 class CreateConfigForAPushinhoChannel(TembaTest):
-
     def test_method_for_create_a_config_for_pushinho_channel(self):
         data = {
             "main_icon_color": "#18fae2",
@@ -229,12 +266,23 @@ class CreateConfigForAPushinhoChannel(TembaTest):
         self.assertEqual(config.get("main_icon_color"), data.get("main_icon_color"))
         self.assertEqual(config.get("chat_icon_color"), data.get("chat_icon_color"))
         self.assertEqual(config.get("chat_icon_url"), chat_icon_url)
-        self.assertEqual(config.get("chat_push_message_color"), data.get("chat_push_message_color"))
-        self.assertEqual(config.get("chat_push_text_color"), data.get("chat_push_text_color"))
-        self.assertEqual(config.get("chat_user_text_color"), data.get("chat_user_text_color"))
+        self.assertEqual(
+            config.get("chat_push_message_color"), data.get("chat_push_message_color")
+        )
+        self.assertEqual(
+            config.get("chat_push_text_color"), data.get("chat_push_text_color")
+        )
+        self.assertEqual(
+            config.get("chat_user_text_color"), data.get("chat_user_text_color")
+        )
         self.assertEqual(config.get("auto_open"), "false")
-        self.assertEqual(config.get(Channel.CONFIG_SEND_URL), settings.PUSH_WEB_SOCKET_URL)
+        self.assertEqual(
+            config.get(Channel.CONFIG_SEND_URL), settings.PUSH_WEB_SOCKET_URL
+        )
         self.assertEqual(config.get(Channel.CONFIG_SEND_METHOD), "POST")
-        self.assertEqual(config.get(Channel.CONFIG_CONTENT_TYPE), Channel.CONTENT_TYPES.get("CONTENT_TYPE_JSON"))
+        self.assertEqual(
+            config.get(Channel.CONFIG_CONTENT_TYPE),
+            Channel.CONTENT_TYPES.get("CONTENT_TYPE_JSON"),
+        )
         self.assertEqual(config.get(Channel.CONFIG_MAX_LENGTH), 640)
         self.assertEqual(config.get(Channel.CONFIG_ENCODING), Channel.ENCODING_DEFAULT)
